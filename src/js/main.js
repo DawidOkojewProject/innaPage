f337eff0a82c2cdac7b59206f79a8b8e360ffce3
Dawid <dawid.okojew@o2.pl>
2024-01-22 17:31:10 +0100

    changed question section in javascript

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,13 @@
 let question = document.querySelector('.question_one')
 let answer = document.querySelector('.answer_one')
+let questions = document.querySelectorAll('.question')
 
-question.addEventListener('click', () => {
-	answer.classList.toggle('display-yes')
-})
+console.log(questions)
+
+const addClass = function () {
+	questions.forEach(question, () => {
+		question.nextElementSibling.classList.toggle('display_yes')
+	})
+}
+
+addClass()
