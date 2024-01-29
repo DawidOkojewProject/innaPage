da16585125e63a5f50885b261a092b5d08a9b5b8
dawidek14472 <dawid.okojew@o2.pl>
2024-01-29 19:43:38 +0100

    added input text for name product in cashFlow section

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,9 @@
 let question = document.querySelector('.question_one')
 let answer = document.querySelector('.answer_one')
+let questions = document.querySelectorAll('.question')
 
 question.addEventListener('click', () => {
 	answer.classList.toggle('display-yes')
 })
+
+console.log(questions)
